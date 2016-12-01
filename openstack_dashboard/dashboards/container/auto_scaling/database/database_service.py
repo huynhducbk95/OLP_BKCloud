b18be200316e719919c294f255870da1da88c955
huynhducbk95 <huynhducpro95@gmail.com>
2016-12-01 10:17:08 +0700

    create database

---
+++ openstack_dashboard/dashboards/container/auto_scaling/database/database_service.py
@@ -0,0 +1,38 @@
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, Column, String, Float
+from sqlalchemy.orm import sessionmaker, scoped_session
+import os.path
+
+Base = declarative_base()
+CURRENT_FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+class Rule(Base):
+    __tablename__ = 'rule'
+
+    id = Column(Integer, primary_key=True)
+    metric = Column(String(50))
+    upper_threshold = Column(Float)
+    lower_threshold = Column(Float)
+    node_up = Column(Integer)
+    node_down = Column(Integer)
+
+    def __repr__(self):
+        return self.id
+
+
+engine = create_engine(
+    'sqlite:///' + CURRENT_FOLDER_PATH + '/service.sqlite', echo=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+Base.metadata.create_all(engine)
+
+
+def get_rule_list():
+    rule_list = db_session.query(Rule).all()
+    return rule_list
